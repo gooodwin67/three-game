93680eded16bb1b8d767184d0689ad20784decd8
sn67@inbox.ru <sn67@inbox.ru>
2024-12-17 20:11:52 +0300

    init

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// npx vite --host
+
 import * as THREE from 'three';
 
 import { OrbitControls } from "three/addons/controls/OrbitControls";
@@ -46,7 +48,10 @@ scene.add(player);
 camera.position.z = 5;
 
 let mouse = new THREE.Vector2();
+let raycaster = new THREE.Raycaster();
 
+let targetPosition = new THREE.Vector3(0, 0, 0);
+const speed = 0.1;
 
 function init() {
 
@@ -55,10 +60,18 @@ function init() {
 
 function animate() {
 
- player.position.x = mouse.x * 2;
+  const direction = new THREE.Vector3().subVectors(targetPosition, player.position).normalize();
 
- // controls.update();
- renderer.render(scene, camera);
+  player.position.addScaledVector(direction, speed);
+
+  if (player.position.distanceTo(targetPosition) <= speed) {
+    player.position.copy(targetPosition);
+  }
+
+
+
+  // controls.update();
+  renderer.render(scene, camera);
 
 }
 renderer.setAnimationLoop(animate);
@@ -66,114 +79,38 @@ renderer.setAnimationLoop(animate);
 
 
 document.addEventListener('touchmove', onDocumentTouchDown);
+document.addEventListener('touchstart', onDocumentTouchDown);
+document.addEventListener('touchend', onDocumentTouchEnd);
+
+function onDocumentTouchEnd(e) {
+  console.log('end');
+  targetPosition = player.position;
+
+}
 
 function onDocumentTouchDown(e) {
 
 
- e = e.changedTouches[0];
+  e = e.changedTouches[0];
 
- var rect = renderer.domElement.getBoundingClientRect();
+  var rect = renderer.domElement.getBoundingClientRect();
 
- mouse.x = ((e.clientX - rect.left) / rect.width) * 2 - 1;
- mouse.y = - ((e.clientY - rect.top) / rect.height) * 2 + 1;
+  mouse.x = ((e.clientX - rect.left) / rect.width) * 2 - 1;
+  mouse.y = - ((e.clientY - rect.top) / rect.height) * 2 + 1;
+
+  raycaster.setFromCamera(mouse, camera);
+
+  plane.geometry.computeBoundingBox();
+  var box1 = plane.geometry.boundingBox.clone();
+  box1.applyMatrix4(plane.matrixWorld);
+
+  let intersects = raycaster.ray.intersectBox(box1, new THREE.Vector3());
+
+
+  targetPosition = new THREE.Vector3(intersects.x, player.position.y, player.position.z);
+
+
+
+  // player.position.x = intersects.x;
 
 }
-
-/*
-function onDocumentMouseDown( event ) {
- mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
- mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
- 
- raycaster.setFromCamera( mouse, camera );
- let intersects = raycaster.intersectObjects( [].concat(...mas) );
- 
- if (play == 1 && intersects.length > 0 && moveDown == 0 && moveUp == 0 && moveLeft == 0 && moveRight == 0) {
-   
-   let posInMasY = intersects[0].object.i;
-   let posInMasX = intersects[0].object.j;
-
-   if (posInMasY < 3 && mas[parseInt(posInMasY)+1][posInMasX].p == 16) {
-     showClearBlock(intersects[0].object)
-     let posY = intersects[0].object.position.y;
-     let posYempty = mas[parseInt(posInMasY)+1][posInMasX].position.y;
-     
-     obj1 = intersects[0];
-     obj2 = posYempty;
-     moveDown = 1;
-
-     mas[parseInt(posInMasY)+1][posInMasX].position.y = posY;
-     
-     let tempI1 = mas[parseInt(posInMasY)][posInMasX].i;
-     let tempI2 = mas[parseInt(posInMasY)+1][posInMasX].i
-     mas[parseInt(posInMasY)][posInMasX].i = tempI2;
-     mas[parseInt(posInMasY)+1][posInMasX].i = tempI1;
-     
-    [mas[parseInt(posInMasY)][posInMasX], mas[parseInt(posInMasY)+1][posInMasX]] 
-     = [mas[parseInt(posInMasY)+1][posInMasX], mas[parseInt(posInMasY)][posInMasX]];
-   }
-   
-   
-   
-   
-   else if (posInMasY > 0 && mas[parseInt(posInMasY)-1][posInMasX].p == 16) {
-     showClearBlock(intersects[0].object)
-     let posY = intersects[0].object.position.y;
-     let posYempty = mas[parseInt(posInMasY)-1][posInMasX].position.y;
-     
-     obj1 = intersects[0];
-     obj2 = posYempty;
-     moveUp = 1;
-     
-     mas[parseInt(posInMasY)-1][posInMasX].position.y = posY;
-     
-     let tempI1 = mas[parseInt(posInMasY)][posInMasX].i;
-     let tempI2 = mas[parseInt(posInMasY)-1][posInMasX].i
-     mas[parseInt(posInMasY)][posInMasX].i = tempI2;
-     mas[parseInt(posInMasY)-1][posInMasX].i = tempI1;
-     
-     [mas[parseInt(posInMasY)][posInMasX], mas[parseInt(posInMasY)-1][posInMasX]] 
-      = [mas[parseInt(posInMasY)-1][posInMasX], mas[parseInt(posInMasY)][posInMasX]];
-     
-   }
-   else if (posInMasX < 3 && mas[posInMasY][parseInt(posInMasX)+1].p == 16) {
-     showClearBlock(intersects[0].object)
-     let posX = intersects[0].object.position.x;
-     let posXempty = mas[posInMasY][parseInt(posInMasX)+1].position.x;
-     
-     obj1 = intersects[0];
-     obj2 = posXempty;
-     moveRight = 1;
-     
-     mas[posInMasY][parseInt(posInMasX)+1].position.x = posX;
-     
-     let tempI1 = mas[parseInt(posInMasY)][posInMasX].j;
-     let tempI2 = mas[posInMasY][parseInt(posInMasX)+1].j;
-     mas[parseInt(posInMasY)][posInMasX].j = tempI2;
-     mas[posInMasY][parseInt(posInMasX)+1].j = tempI1;
-     
-    [mas[parseInt(posInMasY)][posInMasX], mas[posInMasY][parseInt(posInMasX)+1]] 
-     = [mas[posInMasY][parseInt(posInMasX)+1], mas[parseInt(posInMasY)][posInMasX]];
-   }
-   
-   else if (posInMasX > 0 && mas[posInMasY][parseInt(posInMasX)-1].p == 16) {
-     showClearBlock(intersects[0].object)
-     let posX = intersects[0].object.position.x;
-     let posXempty = mas[posInMasY][parseInt(posInMasX)-1].position.x;
-     
-     obj1 = intersects[0];
-     obj2 = posXempty;
-     moveLeft = 1;
-     
-     mas[posInMasY][parseInt(posInMasX)-1].position.x = posX;
-     
-     let tempI1 = mas[parseInt(posInMasY)][posInMasX].j;
-     let tempI2 = mas[posInMasY][parseInt(posInMasX)-1].j;
-     mas[parseInt(posInMasY)][posInMasX].j = tempI2;
-     mas[posInMasY][parseInt(posInMasX)-1].j = tempI1;
-     
-    [mas[parseInt(posInMasY)][posInMasX], mas[posInMasY][parseInt(posInMasX)-1]] 
-     = [mas[posInMasY][parseInt(posInMasX)-1], mas[parseInt(posInMasY)][posInMasX]];
-   }
-  win();
- }
-}*/

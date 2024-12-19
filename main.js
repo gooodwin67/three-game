eec4d1c928ffe244315bb2b6fc8d02d19150759c
sn67@inbox.ru <sn67@inbox.ru>
2024-12-19 21:43:51 +0300

    rapier test

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 import * as THREE from 'three';
 import RAPIER from '@dimforge/rapier3d-compat';
 
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 
 import { OrbitControls } from "three/addons/controls/OrbitControls";
 
@@ -13,7 +15,7 @@ const world = new RAPIER.World(new RAPIER.Vector3(0, -9.81, 0));
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 4, 0);
+camera.position.set(2, 8, 0);
 
 
 const renderer = new THREE.WebGLRenderer();
@@ -21,9 +23,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 /*/////////////////////////////////////////////////////*/
 
-// let controls = new OrbitControls(camera, renderer.domElement);
-// controls.enableDamping = true;
-// controls.target.set(0, 0, 0);
+let controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.target.set(0, 0, 0);
 /*/////////////////////////////////////////////////////*/
 
 const ambientLight = new THREE.AmbientLight(0xaaaaaa); // soft white light
@@ -67,28 +69,45 @@ let raycaster = new THREE.Raycaster;
 let geometryPlane = new THREE.BoxGeometry(3, 0.2, 7);
 let materialPlane = new THREE.MeshPhongMaterial({ color: 0xffffff, side: THREE.DoubleSide })
 plane = new THREE.Mesh(geometryPlane, materialPlane);
+plane.userData.mass = 0;
+plane.userData.position = new THREE.Vector3(0, 0, 0);
+plane.userData.param = new THREE.Vector3(1.5, 0.1, 3.5);
 plane.receiveShadow = true;
 scene.add(plane);
 
-const planeBody = world.createRigidBody(RAPIER.RigidBodyDesc.fixed())
-const planeShape = RAPIER.ColliderDesc.cuboid(1.5, 0.1, 3.5)
-world.createCollider(planeShape, planeBody)
-dynamicBodies.push([plane, planeBody])
+// const planeBody = world.createRigidBody(RAPIER.RigidBodyDesc.fixed())
+// const planeShape = RAPIER.ColliderDesc.cuboid(1.5, 0.1, 3.5)
+// world.createCollider(planeShape, planeBody)
+// dynamicBodies.push([plane, planeBody])
 
 
 let geometryPlayer2 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 let materialPlayer2 = new THREE.MeshPhongMaterial({ color: 0x0000ff, side: THREE.DoubleSide })
 player2 = new THREE.Mesh(geometryPlayer2, materialPlayer2);
+player2.userData.mass = 1;
+player2.userData.position = new RAPIER.Vector3(0, 3, 0);
+player2.userData.param = new THREE.Vector3(0.25, 0.25, 0.25);
+player2.position.set(0, 2, 0)
 player2.receiveShadow = true;
 scene.add(player2);
 
-const player2Body = world.createRigidBody(RAPIER.RigidBodyDesc.dynamic().setTranslation(1, 3, 0).setCanSleep(false))
-const player2Shape = RAPIER.ColliderDesc.cuboid(0.25, 0.25, 0.25).setMass(1).setRestitution(0).setFriction(0);
-world.createCollider(player2Shape, player2Body)
-dynamicBodies.push([player2, player2Body])
+// const player2Body = world.createRigidBody(RAPIER.RigidBodyDesc.dynamic().setTranslation(1, 3, 0).setCanSleep(false))
+// const player2Shape = RAPIER.ColliderDesc.cuboid(0.25, 0.25, 0.25).setMass(1).setRestitution(0).setFriction(0);
+// world.createCollider(player2Shape, player2Body)
+// dynamicBodies.push([player2, player2Body])
 
 
 
+const gltfLoader = new GLTFLoader();
+const url = 'test-blend.glb';
+gltfLoader.load(url, (gltf) => {
+  const root = gltf.scene;
+  scene.add(root);
+  addPhysicsToAllObjects3D();
+});
+
+
+addPhysicsToAllObjects();
 
 
 
@@ -102,6 +121,17 @@ function animate() {
   camera.lookAt(0, 0, 0);
 
   world.step();
+
+  // world.bodies.forEach((body) => {
+  //   const position = body.translation();
+  //   const rotation = body.rotation();
+
+  //   const mesh = scene.getObjectById(body.userData.id); // Предполагаем, что id связан с объектом
+  //   if (mesh) {
+  //     mesh.position.set(position.x, position.y, position.z);
+  //     mesh.quaternion.set(rotation.x, rotation.y, rotation.z, rotation.w);
+  //   }
+  // });
 
   for (let i = 0, n = dynamicBodies.length; i < n; i++) {
     dynamicBodies[i][0].position.copy(dynamicBodies[i][1].translation())
@@ -118,9 +148,9 @@ function animate() {
 }
 renderer.setAnimationLoop(animate);
 
-document.addEventListener('touchend', onTouchEnd);
-document.addEventListener('touchstart', onTouchMove);
-document.addEventListener('touchmove', onTouchMove);
+// document.addEventListener('touchend', onTouchEnd);
+// document.addEventListener('touchstart', onTouchMove);
+// document.addEventListener('touchmove', onTouchMove);
 
 function onTouchEnd() {
   if (playerOnGround) {
@@ -158,4 +188,60 @@ function onTouchMove(e) {
 }
 
 
+function addPhysicsToAllObjects() {
+  scene.traverse((object) => {
+    if (object.isMesh) {
+      // Создаем Collider для каждого объекта
+      // const geometry = object.geometry;
+      // const shape = RAPIER.ColliderDesc.trimesh(geometry.attributes.position.array, geometry.index.array);
 
+      // const rigidBody = RAPIER.RigidBodyDesc.dynamic()
+      //   .setTranslation(object.position.x, object.position.y, object.position.z)
+      //   .setRotation(object.quaternion.x, object.quaternion.y, object.quaternion.z, object.quaternion.w);
+
+      // const body = world.createRigidBody(rigidBody);
+      // body.userData = { id: Math.random() }
+      // world.createCollider(shape, body);
+      const body = world.createRigidBody(RAPIER.RigidBodyDesc.dynamic().setTranslation(object.userData.position.x, object.userData.position.y, object.userData.position.z).setCanSleep(false))
+      const shape = RAPIER.ColliderDesc.cuboid(object.userData.param.x, object.userData.param.y, object.userData.param.z).setMass(object.userData.mass).setRestitution(0).setFriction(0);
+      body.userData = { id: Math.random() }
+      world.createCollider(shape, body)
+      dynamicBodies.push([object, body])
+    }
+  });
+}
+
+
+
+function addPhysicsToAllObjects3D() {
+
+  scene.children[3].children.forEach((object) => {
+    if (object.isMesh) {
+      console.log(getObjectSize(object))
+      const body = world.createRigidBody(RAPIER.RigidBodyDesc.dynamic().setTranslation(object.position.x, object.position.y, object.position.z).setCanSleep(false))
+      const shape = RAPIER.ColliderDesc.cuboid(getObjectSize(object).x / 3 / 2, getObjectSize(object).y / 3 / 2, getObjectSize(object).z / 3 / 2).setMass(1).setRestitution(0).setFriction(0);
+      body.userData = { id: Math.random() }
+      world.createCollider(shape, body)
+      dynamicBodies.push([object, body])
+    }
+  });
+}
+
+function getObjectSize(object) {
+  // Убедимся, что геометрия существует
+  if (object.geometry) {
+    // Вычисляем bounding box
+    object.geometry.computeBoundingBox();
+    const boundingBox = object.geometry.boundingBox;
+
+    // Получаем ширину (размер по оси X)
+    const width = boundingBox.max.x - boundingBox.min.x;
+    const height = boundingBox.max.y - boundingBox.min.y;
+    const depth = boundingBox.max.z - boundingBox.min.z;
+
+
+
+    return new THREE.Vector3(width, height, depth);
+  }
+  return null; // Если геометрия не найдена
+}
